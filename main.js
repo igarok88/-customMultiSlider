8d731fd2397d35e274351a51e6943e4a0908d81e
igor <usd.wmz@gmail.com>
2021-09-20 11:01:39 +0300

    add multi slider, append dots in slide-container

--- main.js
+++ main.js
@@ -1,46 +1,68 @@
 window.addEventListener("DOMContentLoaded", () => {
-	let dots = document.querySelectorAll(".dots-item"),
-		dotsArea = document.querySelector(".dots-block"),
-		slides = document.querySelectorAll(".slides-item"),
-		prevBtn = document.querySelector(".left-button"),
-		nextBtn = document.querySelector(".right-button"),
-		slideIndex = 1;
+	let slideContainers = document.querySelectorAll(".slide-container");
 
-	showSlides(slideIndex);
+	slideContainers.forEach((slideContainer) => {
+		// setInterval(() => {
+		// 	plusSlides(1);
+		// }, 1500);
+		console.log(slideContainers[0]);
+		let div = document.createElement("div");
+		div.classList.add("dots-block");
+		slideContainer.appendChild(div);
 
-	function showSlides(n) {
-		if (n < 1) {
-			slideIndex = slides.length;
-		} else if (n > slides.length) {
+		let dotsArea = slideContainer.querySelector(".dots-block"),
+			slides = slideContainer.querySelectorAll(".slides-item");
+
+		for (let i = 0; i < slides.length; i++) {
+			let div = document.createElement("div");
+			div.classList.add("dots-item");
+			dotsArea.appendChild(div);
+		}
+
+		let dots = slideContainer.querySelectorAll(".dots-item"),
+			prevBtn = slideContainer.querySelector(".left-button"),
+			nextBtn = slideContainer.querySelector(".right-button"),
 			slideIndex = 1;
-		}
-		slides.forEach((slide) => {
-			slide.style.display = "none";
-		});
-		dots.forEach((dot) => {
-			dot.classList.remove("active");
-		});
-		slides[slideIndex - 1].style.display = "block";
-		dots[slideIndex - 1].classList.add("active");
-	}
 
-	function plusSlides(n) {
-		showSlides((slideIndex += n));
-	}
-	function currentSlide(n) {
-		showSlides((slideIndex = n));
-	}
-	prevBtn.addEventListener("click", () => {
-		plusSlides(-1);
-	});
-	nextBtn.addEventListener("click", () => {
-		plusSlides(1);
-	});
-	dotsArea.addEventListener("click", (e) => {
-		for (let i = 0; i < dots.length + 1; i++) {
-			if (e.target.classList.contains("dots-item") && e.target == dots[i - 1]) {
-				currentSlide(i);
+		showSlides(slideIndex);
+
+		function showSlides(n) {
+			if (n < 1) {
+				slideIndex = slides.length;
+			} else if (n > slides.length) {
+				slideIndex = 1;
 			}
+			slides.forEach((slide) => {
+				slide.style.display = "none";
+			});
+			dots.forEach((dot) => {
+				dot.classList.remove("active");
+			});
+			slides[slideIndex - 1].style.display = "block";
+			dots[slideIndex - 1].classList.add("active");
 		}
+
+		function plusSlides(n) {
+			showSlides((slideIndex += n));
+		}
+		function currentSlide(n) {
+			showSlides((slideIndex = n));
+		}
+		prevBtn.addEventListener("click", () => {
+			plusSlides(-1);
+		});
+		nextBtn.addEventListener("click", () => {
+			plusSlides(1);
+		});
+		dotsArea.addEventListener("click", (e) => {
+			for (let i = 0; i < dots.length + 1; i++) {
+				if (
+					e.target.classList.contains("dots-item") &&
+					e.target == dots[i - 1]
+				) {
+					currentSlide(i);
+				}
+			}
+		});
 	});
 });
